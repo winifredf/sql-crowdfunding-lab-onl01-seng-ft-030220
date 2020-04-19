f7986f54ce305ec62f292b8c5868a00c644245e4
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-19 00:54:16 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,3 +4,14 @@ title TEXT,
 category TEXT,
 funding_goal INTEGER
 );
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age TEXT
+);
+
+CREATE TABLE pledge (
+id INTEGER PRIMARY KEY,
+amount INTEGER
+);
